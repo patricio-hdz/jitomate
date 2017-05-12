229ec94fa9e87748d103b67a6fbd0bbd46895c32
patricio-hdz <v.p.h.d@hotmail.com>
2017-05-11 20:32:58 -0500

    Create luigi.py

---
+++ scripts/luigi.py
@@ -0,0 +1,14 @@
+lang-python
+import luigi
+
+class HelloWorld(luigi.Task):
+    def requires(self):
+        return None
+    def output(self):
+        return luigi.LocalTarget('helloworld.txt')
+    def run(self):
+        with self.output().open('w') as outfile:
+            outfile.write('Hello World!\n')
+
+if __name__ == '__main__':
+    luigi.run()
